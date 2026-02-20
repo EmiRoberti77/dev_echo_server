cfa483b40929b6fd630ffa52d54561c32b73d4a9
emi <emiroberti@icloud.com>
2026-02-20 19:46:22 +0000

    link probe buffer

--- src/py/links/link.py
+++ src/py/links/link.py
@@ -8,16 +8,24 @@ usb       -> linked to wifi or ethernet
 """
 import argparse
 import asyncio
+from collections import deque
 from dataclasses import dataclass
 from datetime import datetime
+from re import S
 from typing import Optional, Tuple
+from xxlimited import Str
 import logger as l
+import time
 
 SERVER_HOST = ('34.13.59.163', 8010)
 BUFFER = 65536
 
 def ts():
     return datetime.now().isoformat()
+
+def build_probe_payload(size_mb:int=4):
+    payload = b''.join(b'x' * (size_mb * 1024 * 1024))
+    return payload
 
 @dataclass
 class Connection:
@@ -27,6 +35,66 @@ class Connection:
     link_host:str
     link_port:int
     link_name:str
+
+@dataclass
+class ProbeResult:
+    ok:bool
+    rtt_ms:float | None
+    last_ok: float
+
+class ProbeBuffer:
+    def __init__(self):
+        self.max_capacity = 10
+        self.probe_buffer = deque[float](maxlen=self.max_capacity)
+    
+    async def avg_rtt(self, rtt_ms:float, verbose:bool=False):
+        self.probe_buffer.append(rtt_ms)
+        if verbose: l.log(f'probe_buffer:{self.probe_buffer}')
+        if self.probe_buffer:
+            avg_rtt = sum(self.probe_buffer) / len(self.probe_buffer)
+        else:
+            avg_rtt = 0
+        return avg_rtt
+        
+
+async def probe_loop(connection:Connection, interval:float=2.0, timeout:float=1.0, large_probe:bool=False):
+    last_ok = 0.0
+    probe_buffer = ProbeBuffer()
+    try:
+        while True:
+            l.log(f'probe_loop:starting_test:{connection}')
+            reader, writer = await asyncio.open_connection(
+                host=connection.target_host,
+                port=connection.target_port,
+                local_addr=(connection.link_host, 0) 
+            )
+            t0 = time.perf_counter()
+            try:
+                payload = Optional[str] = None
+                if large_probe:
+                    payload = build_probe_payload(4)
+                else:
+                    payload = f'PING-{t0}\n'.encode()
+                writer.write(payload)
+                await writer.drain()
+            except:
+                l.log('probe_loop:ping:error:1006', l.LogLevel.ERR)
+            try:
+                response = await asyncio.wait_for(reader.readline(), timeout=timeout)
+                t1 = time.perf_counter()
+                l.log(f'probe_loop:response:{response.decode()}')
+                if large_probe:
+                    if not response.decode().startswith('PING'):
+                        l.log('probe_loop:incorrect_response', l.LogLevel.WARN)
+                rtt_ms = (t1 - t0) * 1000
+                avg_rtt = await probe_buffer.avg_rtt(rtt_ms=rtt_ms)
+                last_ok = time.time()
+                l.log(f'probe_loop:rtt_ms:{rtt_ms} avg_rtt:{avg_rtt}')
+                await asyncio.sleep(interval)
+            except (asyncio.TimeoutError, ConnectionError) as err:
+                l.log(f'probe_loop:response:error:1007:{str(err)}', l.LogLevel.ERR)
+    except Exception as err:
+        l.log(f'probe_loop error:1005:{str(err)}',l.LogLevel.ERR)
 
 async def close_writer(w:asyncio.StreamWriter):
     try:
@@ -87,12 +155,17 @@ async def main(connection: Connection):
     l.log(connection)
     uhost, uport = connection.target_host, connection.target_port
     bind_addr = (connection.link_host, connection.link_port) if connection.link_host else None
+    # start server
     server = await asyncio.start_server(
         lambda r, w: handle_client(r, w, uhost, uport, bind_addr),
         host='0.0.0.0',
         port=connection.local_port,
         reuse_address=True
     )
+    # start probe monitoring
+    probe_loop_task = asyncio.create_task(probe_loop(connection, large_probe=True))
+
+    #keep server alive
     async with server:
         await server.serve_forever()
 
@@ -114,4 +187,7 @@ if __name__ == "__main__":
         link_port=0,
         link_name=args.name
     )
-    asyncio.run(main(connection))
+    try:
+        asyncio.run(main(connection))
+    except KeyboardInterrupt:
+        pass
